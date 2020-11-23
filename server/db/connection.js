d5942182377808524c2430fd0a9bdd86ab8f22f3
githubmave <lidanmei@yahoo.com>
2020-11-23 22:07:45 +1300

    add to connection, routes

--- server/db/connection.js
+++ server/db/connection.js
@@ -5,15 +5,35 @@ const config = require('./knexfile')
 const connection = knex(config[env])
 
 module.exports = {
-       getTodos: getTodos
+       getTodos: getTodos,
+       getTodoById: getTodoById,
+       addTodo: addTodo
 }
 
 
 function getTodos(db = connection){
-   
-
-     return db('todos')
+        return db('todos')
          .select()
-    
 }
 
+function getTodoById(id,db = connection){
+       return db('todos')
+        .select()
+        .where("id",id)
+        .first()
+        .then((todoIt) =>{
+           return todoIt
+
+        })
+}
+
+function addTodo(todoName, priority, completed, db= connection) {
+       let todoRw = {};
+       todoRw.todoName = todoName;
+       todoRw.priority = priority;
+       todoRw.completed = completed;
+   
+     console.log(todoRw);
+     return db("todos")
+     .insert(todoRw);
+}
--- server/routes/todos.js
+++ server/routes/todos.js
@@ -2,13 +2,13 @@
 const express=require("express")
 const router =express.Router()
 
-const {getTodos} =require ("../db/connection")
+const {getTodos,getTodoById} =require ("../db/connection")
 
 router.get('/' ,(req, res)=>{
 
     getTodos()
      .then((todoLs) =>{
-        console.log("It is my list :",todoLs)
+        //console.log("It is my list :",todoLs)
          res.json(todoLs)
      })
      .catch((err) => {
@@ -16,5 +16,27 @@ router.get('/' ,(req, res)=>{
       res.status(500);
     });
 })
+
+router.get('/:id' ,(req, res)=>{
+    let id =req.params.id
+
+    getTodoById(id)
+     .then((todoIt) =>{
+        //console.log("It is my list :",todoLs)
+         res.json(todoIt)
+     })
+     .catch((err) => {
+      console.log(err);
+      res.status(500);
+    });
+})
+
+router.get( 'update/:id',(req,res)=>{
+
+    res.json()
+})
+
+
+
 
 module.exports = router;    
