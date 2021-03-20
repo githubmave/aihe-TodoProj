954d377048098b799f6d46c8aff1cab040e9276b
githubmave <lidanmei@yahoo.com>
2021-03-20 20:16:59 +1300

    change: TodoItem

--- client/components/TodoItem.jsx
+++ client/components/TodoItem.jsx
@@ -1,41 +1,21 @@
 import React,{useState} from 'react'
 import {connect} from 'react-redux'
-import {updateTodo,deleteTodo} from 'apis/api'
+
 
 
 function TodoItem(props){
 
-     const [editable, setEditable]=useState(false)
+     const [editable,setEditable] =useState(false)
      const [todoNam,setTodoNam] =useState(props.todoItem.todoName)
-     const [todoItm,setTodoItm]=useState(props.todoItem)
 
-   function handleSubmit(){
-      todoItm.todoName=todoNam;
-      
-      return updateTodo(,dispatch)
-   }
+
+   
 
    return(
 
     <> 
-       {!editable &&
-        <li className="new-todo" onDoubleClick={e => setEditable(true)}>{todoNam}
-       
-        <button className="destroy" onClick={}></button>
-        </li>
-       }
-
-       {editable &&
-        
-         <form onSubmit={handleSubmit}>
-          <li className="new-todo" type="text" onChange={e => setTodoNam(e.target.value)}>{todoName}
-          <button className="new-todo"></button>
-          </li>
-         </form>
-
-       }
-       
-      
+    
+    
     </>
 
    )
