8fbd401cb0a07981f0f952b8935caaa66ef4cd2c
githubmave <lidanmei@yahoo.com>
2021-03-22 11:27:29 +1300

    changed: TodoItem: onChange() has issue

--- client/components/TodoItem.jsx
+++ client/components/TodoItem.jsx
@@ -1,5 +1,6 @@
 import React,{useState} from 'react'
 import {connect} from 'react-redux'
+import {updateTodo, deleteTodo} from '../apis/api'
 
 
 
@@ -7,15 +8,30 @@ function TodoItem(props){
 
      const [editable,setEditable] =useState(false)
      const [todoNam,setTodoNam] =useState(props.todoItem.todoName)
+     const [todoItm, setTodoItm]=useState(props.todoItem)
 
 
-   
+     const handleSubmit = () =>{
+      id=todoItm.id
+      todoItm.todoName=todoNam
 
+         return  updateTodo(id,todoItm,props.dispatch)
+     }
+
+      
+     
    return(
 
     <> 
-    
-    
+         <form onSubmit={handleSubmit}>
+            {!editable && 
+                  <input type="text" className="new-todo" value={todoNam} onDoubleClick={e => setEditable(true)}/>
+            }
+            {editable && 
+                  <input type="text" className="new-todo" value={todoNam} onChange={e =>setTodoNam(e.target.value)}/>
+            }
+
+     </form>
     </>
 
    )
